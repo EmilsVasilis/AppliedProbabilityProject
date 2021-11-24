d0f89b6696930f07dc501920dbc7d3a354a0ae1c
EmilsVasilis <emils5264@gmail.com>
2021-11-24 15:32:09 +0000

    First

--- src/gameTable.java
+++ src/gameTable.java
@@ -2,14 +2,17 @@
 public class gameTable {
 
     public static void main(String args[]){
-        int wins = 0;
+        double winnings = 0;
+        double wins = 0;
         for(int i = 0; i < 10000; i++) {
             if (gameOne.playGameOne() == 0){
-                wins += -1;
+                winnings += -1;
             }else{
-                wins += 1;
+                winnings += 1;
+                wins++;
             }
         }
-        System.out.println(wins);
+        System.out.println("Expected winnings for game one: " + winnings/10000 + " euro");
+        System.out.println("Proportions of wins: " + wins / 10000);
     }
 }
